2b5b73a3d9b33ed9eba2a2eda2eeebbd9512f101
Lovro <hodak.lovro@gmail.com>
2022-06-08 19:03:19 +0200

    api env

--- src/useQueryHook.js
+++ src/useQueryHook.js
@@ -9,7 +9,7 @@ export async function getData(
 ) {
   // city is params, else is body, because on BE: router.get("/api/data/:mesto", async (req, res) => {});
   const response = await fetch(
-    `http://localhost:4242/api/data/${city}?startDay=${startDay}&startMonth=${startMonth}&startYear=${startYear}&endDay=${endDay}&endMonth=${endMonth}&endYear=${endYear}`
+    `${process.env.REACT_APP_API_URL}/api/data/${city}?startDay=${startDay}&startMonth=${startMonth}&startYear=${startYear}&endDay=${endDay}&endMonth=${endMonth}&endYear=${endYear}`
   );
 
   const data = await response.json();
